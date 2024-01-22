4a8fafe61d97a681dd6a623dcb4ba413a5db6b2a
ZX291 <121951388+ZX291@users.noreply.github.com>
2024-01-22 21:52:29 +0800

    New branch

---
+++ team2-swap-Development/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, flash, request, redirect, url_for, render_template
+import urllib.request
+import os
+from werkzeug.utils import secure_filename
+
+aapp = Flask(__name__)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ team2-swap-Development/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+from pypdf import PdfReader
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import os
